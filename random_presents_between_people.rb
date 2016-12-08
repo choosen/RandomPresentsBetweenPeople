9fb65e0f3522f29f68895fae2b5642bf9a5e57a9
Piotr Wasiak <piotr.wasiak@netguru.co>
2016-12-09 00:32:41 +0100

    Allow input a length of the parameters to RandomPresentsBetweenPeople

--- random_presents_between_people.rb
+++ random_presents_between_people.rb
@@ -2,8 +2,9 @@
 # nobody will give himself a present and eatch other with somebody.
 class RandomPresentsBetweenPeople
   def initialize(parameters)
-    @names = parameters
-    return if @names.count > 2
+    @names_count = parameters.length if parameters.class == Array
+    @names_count = parameters if parameters.class == Fixnum
+    return if @names_count > 2
     raise 'Too little names for suffiecient with requirements for draw. ' \
              'Maybe you missed space between names?'
   end
@@ -15,15 +16,12 @@ class RandomPresentsBetweenPeople
   private
 
   def all_combinations
-    initial_combination = Array(0..@names.count - 1)
+    initial_combination = Array(0..@names_count - 1)
     initial_combination.permutation.to_a
   end
 
   def correct_combinations
-    return [] if @names.empty? || @names.count < 3
-    all_combinations.reject do |combination|
-      check_all_conditions(combination)
-    end
+    all_combinations.reject { |combination| check_all_conditions(combination) }
   end
 
   def check_all_conditions(combination)
